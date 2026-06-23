0331d13bfedeabfa08112da22fa817991b19eb20
drpashaac-cyber <drpasha.ac@gmail.com>
2026-06-23 08:57:50 +0330

    fix: use redirects for /os, /os/dashboard and /os/owner

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,33 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 
-  async rewrites() {
+  async redirects() {
     return [
+      {
+        source: '/os',
+        destination: '/fa',
+        permanent: true,
+      },
       {
         source: '/os/dashboard',
         destination: 'https://dash.appido.io',
+        permanent: true,
       },
       {
         source: '/os/dashboard/:path*',
         destination: 'https://dash.appido.io/:path*',
+        permanent: true,
       },
       {
         source: '/os/owner',
         destination: 'https://owner.appido.io',
+        permanent: true,
       },
       {
         source: '/os/owner/:path*',
         destination: 'https://owner.appido.io/:path*',
+        permanent: true,
       },
     ];
   },
